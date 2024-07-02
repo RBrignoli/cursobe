870523508d66b8e30cbefbec9f5b912fc3b66e1c
rbrignoli <ramon.brignoli@gmail.com>
2024-07-02 15:25:14 -0300

    add model layer

---
+++ db.js
@@ -0,0 +1,23 @@
+const { Client } = require('pg')
+require("dotenv").config();
+
+const client = new Client({
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+})
+
+const connectDB = async () => {
+    client
+    .connect()
+    .then(() => {
+        console.log('Connected to PostgreSQL database');
+    })
+    .catch((err) => {
+        console.error('Error connecting to PostgreSQL database', err);
+    });
+};
+
+module.exports = { connectDB }
