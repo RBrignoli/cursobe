7272b42dfff4c46e9b7d147d1515a5372899d138
rbrignoli <ramon.brignoli@gmail.com>
2024-07-11 11:25:19 -0300

    add rotas crud

--- rotas.js
+++ rotas.js
@@ -5,6 +5,12 @@ const router = express.Router();
 
 
 router.get("/", controlador.listUsers);
+router.get("/:id", controlador.getUser);
+router.post("/", controlador.createUser);
+router.post("/:id", controlador.updateUser);
+router.delete("/:id", controlador.deleteUser);
+
+
 router.get("/setup", setup);
 router.get("/drop", setup);
 
