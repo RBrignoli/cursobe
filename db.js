223440151d5f2d51ba9b1e04c90dbb032b4a8497
rbrignoli <ramon.brignoli@gmail.com>
2024-07-11 11:21:21 -0300

    add setup and drop table

--- db.js
+++ db.js
@@ -22,12 +22,23 @@ const connectDB = async () => {
 
 const setup = async (req, res) => {
     try {   
-        const data = await client.query('CREATE TABLE usuarios( id SERIAL PRIMARY KEY, name VARCHAR(100) )')
+        const data = await client.query('CREATE TABLE usuarios( id SERIAL PRIMARY KEY, name VARCHAR(100), email VARCHAR(50), password VARCHAR(100) )')
         res.status(200).json({msg: 'table created'});
     } catch (err) {
         console.error('Error executing query', err);
         res.status(500)
     }
 };
+
+const drop = async (req, res) => {
+    try {   
+        const data = await client.query('DROP TABLE usuarios')
+        res.status(200).json({msg: 'table created'});
+    } catch (err) {
+        console.error('Error executing query', err);
+        res.status(500)
+    }
+};
+
 
 module.exports = { connectDB, setup, client }
--- rotas.js
+++ rotas.js
@@ -4,8 +4,9 @@ const { setup } = require("./db")
 const router = express.Router();
 
 
-// router.get("/", controlador.listUsers);
+router.get("/", controlador.listUsers);
 router.get("/setup", setup);
+router.get("/drop", setup);
 
 
 module.exports = router;
